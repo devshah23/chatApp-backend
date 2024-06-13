d9ae6b42de1d11cec56dfa4aa95af433fdc1bd1b
devshah23 <way2devs2302@gmail.com>
2024-06-13 11:15:12 +0530

    Initial commit

---
+++ constants/config.js
@@ -0,0 +1,13 @@
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    process.env.CLIENT_URL,
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+const CHATPULSE_TOKEN = "chattu-token";
+
+export { corsOptions, CHATPULSE_TOKEN };
